06d45a15bfb0d213fbac3de08f9c0d61ed727185
Vivek Poddar <vivekimsit@gmail.com>
2019-03-27 12:13:51 +0000

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# open-target
+Open target project from uniprot
+
---
+++ src/main/java/uk/ac/ebi/uniprot/opentargets48/Application.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.uniprot.opentargets48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
